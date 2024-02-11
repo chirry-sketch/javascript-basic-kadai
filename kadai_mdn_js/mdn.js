a6b9cd777b74029d09af0fe24b2f444f88fd6642
chirry-sketch <154793601+chirry-sketch@users.noreply.github.com>
2024-02-11 22:14:07 +0900

    MDN(関数)課題
    課題提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const moonLanding = new Date();
+
+const year = moonLanding.getFullYear();
+const month = moonLanding.getMonth() + 1;
+const day = moonLanding.getDate();
+
+console.log(year + "年" + month + "月" + day + "日");
